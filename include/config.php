711667445ad373b6aa5be8b8373473aab0d4a41f
jhonponsel <68303308+jhonponsel@users.noreply.github.com>
2020-07-15 05:30:40 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,7 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<1','mikhmon>|>opmhppiTpqWXpphTamljb2Fr');
+
+
+
+$data['HelyaONShop.Net'] = array ('1'=>'HelyaONShop.Net!103.220.47.39:4018','HelyaONShop.Net@|@helya','HelyaONShop.Net#|#eZVlo5ynWWNqaWhicQ==','HelyaONShop.Net%HelyaONShop.Net','HelyaONShop.Net^HelyaONShop.Net','HelyaONShop.Net&Rp','HelyaONShop.Net*10','HelyaONShop.Net(2','HelyaONShop.Net)0','HelyaONShop.Net=0','HelyaONShop.Net@!@enable','HelyaONShop.Net<885981042:AAGUA4T-Wib1Dnb1oSbtEFR9JRjhsi2M6D4','HelyaONShop.Net?708827026','HelyaONShop.Net|?enable','HelyaONShop.Net#!#');
